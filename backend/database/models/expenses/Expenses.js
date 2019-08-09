abdb18922fe9776c2e4e7b43f41e577f52531512
omarjandali <omar@omnacore.com>
2019-08-09 12:37:32 -0700

    added the expense model.

---
+++ backend/database/models/expenses/Expenses.js
@@ -0,0 +1,89 @@
+const seq = require('sequelize');
+const { postgres } = require('../../index');
+const { user } = require('./User');
+
+const expense = postgres.define(
+    "expense",
+    {
+        id: {
+            type: seq.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        location: {
+            type: seq.STRING,
+            null: false,
+            require: true,
+            validate: {
+                isAlphanumeric: true,
+                notNull: true
+            }
+        },
+        split: {
+            type: seq.BOOLEAN,
+            null: false,
+            require: true,
+            validate: {
+                notNull: true,
+                // isIn: [['true', 'false']]
+            }
+        },
+        amount: {
+            type: seq.FLOAT,
+            null: false,
+            require: true,
+            validate: {
+                isFloat: true,
+                notNull: true,
+            }
+        },
+        tax: {
+            type: seq.FLOAT,
+            null: false,
+            require: true,
+            validate: {
+                isFloat: true,
+                notNull: true,
+            }
+        },
+        tip: {
+            type: seq.FLOAT,
+            null: false,
+            require: true,
+            validate: {
+                isFloat: true,
+                notNull: true,
+            }
+        },
+        reference: {
+            type: seq.INTEGER,
+            null: false,
+            require: true,
+            validate: {
+                isNumeric: true,
+                notNull: true,
+                len: [10, 12]
+            }
+        },
+    }
+    ,{
+        createdAt:  seq.DATE,
+        updatedAt:  seq.DATE,
+    },
+);
+postgres.sync()
+    .then(() => {
+        console.log("friend table is synced")
+    })
+    .catch((error) => {
+        console.log("caught error with friend:  " + error)
+    });
+
+expense.hasOne(user);
+expense.hasOne(group, {foreignKey: {
+        allowNull: true
+        }
+    }
+);
+
+module.exports.expense = expense;
